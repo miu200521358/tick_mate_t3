89523b281cb84bc36593d5e578b1af68624ed669
miu <38824651+miu200521358@users.noreply.github.com>
2025-04-13 17:42:00 +0900

    fix: アプリタイトルの表記を統一し、インポート順序を整理

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
 import 'dart:async';
+
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart'; // Add this line
-// Use package import for generated file (synthetic package)
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:tick_mate/config/app_config.dart';
 import 'package:tick_mate/config/config_dev.dart';
